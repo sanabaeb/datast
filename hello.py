01c65136e00fdad09d8bc65b8098e6828cbd45f5
main <ezname1219@gmail.com>
2026-09-17 12:50:11 +0900

    Hello

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("Hello, World!")
+# print("Hello, World!")
+
+kakao = ["가나", "다라", "마바", "사아", "자차"]
+print(kakao)
+print(kakao[0])
+print(kakao[4])
+kakao.append(None)
+print(kakao)
+# kakao.append("카타")
+kakao[5] = "카타"
+print(kakao)
